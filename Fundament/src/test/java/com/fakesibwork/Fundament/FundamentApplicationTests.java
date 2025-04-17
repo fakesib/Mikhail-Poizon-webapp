aff861d88bef4296b263da03fa9a9069efd81c07
fakesib <fakesibbb@gmail.com>
2025-04-17 21:17:41 +0300

    Created fundament module

---
+++ Fundament/src/test/java/com/fakesibwork/Fundament/FundamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakesibwork.Fundament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
