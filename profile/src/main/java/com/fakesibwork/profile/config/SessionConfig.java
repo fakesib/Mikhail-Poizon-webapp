aafb8af20e90c8c20fafd0b3088b0775d085d072
fakesib <fakesibbb@gmail.com>
2025-05-22 11:30:26 +0300

    Added session to profile service

---
+++ profile/src/main/java/com/fakesibwork/profile/config/SessionConfig.java
@@ -0,0 +1,9 @@
+package com.fakesibwork.profile.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class SessionConfig {
+}
