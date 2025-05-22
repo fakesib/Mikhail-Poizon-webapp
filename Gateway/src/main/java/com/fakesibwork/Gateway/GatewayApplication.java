61083bd2047493b259178897737a6d73891e0f77
fakesib <fakesibbb@gmail.com>
2025-05-22 11:23:47 +0300

    Fixed name of gateway service

--- Gateway/src/main/java/com/fakesibwork/Gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/fakesibwork/Gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.fakesibwork.Gateway;
+package com.fakesibwork.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Gateway/src/test/java/com/fakesibwork/Gateway/GatewayApplicationTests.java
+++ Gateway/src/test/java/com/fakesibwork/Gateway/GatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.fakesibwork.Gateway;
+package com.fakesibwork.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
