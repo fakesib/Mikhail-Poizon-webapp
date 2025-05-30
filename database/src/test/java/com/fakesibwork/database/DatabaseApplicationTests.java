36535c650347e8c26f0f20e6766ac1a0be15210d
fakesib <fakesibbb@gmail.com>
2025-05-30 11:32:42 +0300

    Added database service

---
+++ database/src/test/java/com/fakesibwork/database/DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakesibwork.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
