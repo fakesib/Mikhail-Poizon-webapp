1e0d5e9eb805695e663b258b1c5096cdd67db334
fakesib <fakesibbb@gmail.com>
2025-08-05 13:13:35 +0300

    Spring security in Feed-service

---
+++ feed-service/src/main/java/com/fakesibwork/feed/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.fakesibwork.feed.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+
+@EnableRedisHttpSession
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) //
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/auth/login")
+                );
+        return http.build();
+    }
+}
