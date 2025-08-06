2518ddc090424d54b9870afcb8369a9cf8dc0650
fakesib <fakesibbb@gmail.com>
2025-08-06 12:45:27 +0300

    Start feed service

---
+++ feed-service/src/main/java/com/fakesibwork/feed/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.fakesibwork.feed.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/feed")
+public class MainController {
+
+    @GetMapping
+    public String mainPage() {
+        return "page.html";
+    }
+}
