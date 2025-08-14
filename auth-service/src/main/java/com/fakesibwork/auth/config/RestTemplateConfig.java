fd2570b543f76eae9b6d99f7a12cc8081fe36b93
fakesib <fakesibbb@gmail.com>
2025-08-14 09:35:40 +0300

    Added JWT to header rest template

---
+++ auth-service/src/main/java/com/fakesibwork/auth/config/RestTemplateConfig.java
@@ -0,0 +1,25 @@
+package com.fakesibwork.auth.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Value("jwt.token")
+    private String token;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add((request, body, execution) -> {
+            if (token != null) {
+                request.getHeaders().setBearerAuth(token);
+            }
+            return execution.execute(request, body);
+        });
+        return restTemplate;
+    }
+}
