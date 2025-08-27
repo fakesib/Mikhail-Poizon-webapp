3c8fe28dd311d58879340e9fbaa0c045498b5ec5
fakesib <fakesibbb@gmail.com>
2025-08-27 16:52:16 +0300

    Fixes in Common library

--- common-lib/src/main/java/dto/UserDto.java
+++ common-lib/src/main/java/dto/UserDto.java
@@ -2,12 +2,17 @@ package dto;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-public class UserDto {
+public class UserDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     private String username;
     private String password;
     private String email;
