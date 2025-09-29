13f359dff14b78c1ce338725ec86d266a2919cf8
fakesib <fakesibwork@gmail.com>
2025-09-29 23:00:01 +0300

    Fixes

--- database-service/src/main/java/com/fakesibwork/database/controller/PostController.java
+++ database-service/src/main/java/com/fakesibwork/database/controller/PostController.java
@@ -4,6 +4,7 @@ import com.fakesibwork.common.dto.PostDto;
 import com.fakesibwork.database.service.PostService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class PostController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<?> createPost(PostDto postDto) {
+    public ResponseEntity<?> createPost(@RequestBody PostDto postDto) {
         try {
             var postId = postService.createPost(postDto);
             return ResponseEntity.ok("Post created, id is: " + postId.toString());
