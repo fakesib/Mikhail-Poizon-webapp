d1adee1b8e362ed5a3ca5d6b22a1d4487c2b7225
Григорьев Александр Александрович <agrigorev@intdigital.ru>
2025-10-13 14:51:22 +0300

    Kafka Binder added

--- mail-service/src/main/java/com/fakesibwork/mail/handlers/ConfirmMailEventHandler.java
+++ mail-service/src/main/java/com/fakesibwork/mail/handlers/ConfirmMailEventHandler.java
@@ -2,21 +2,39 @@ package com.fakesibwork.mail.handlers;
 
 import com.fakesibwork.mail.service.MailService;
 import com.fakesibwork.common.dto.UserDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 @Service
 public class ConfirmMailEventHandler {
 
-    @Autowired
-    private MailService mailService;
+    private static final Logger log = LoggerFactory.getLogger(ConfirmMailEventHandler.class);
 
-    @KafkaListener(topics = "confirm-mail-event-topic")
-    public void handler(UserDto userDto) {
+    private final MailService mailService;
+
+    public ConfirmMailEventHandler(MailService mailService) {
+        this.mailService = mailService;
+    }
+
+    @Bean
+    public Consumer<Message<UserDto>> mailConfirmConsumer() {
+        return this::mailConfirm;
+    }
+
+    public void mailConfirm(Message<UserDto> message) {
+        log.info(String.valueOf(message.getHeaders().get(KafkaHeaders.RECEIVED_PARTITION, Integer.class)));
+        log.info(message.getHeaders().get(KafkaHeaders.OFFSET, String.class));
+        var userDto = message.getPayload();
         Map<String, Object> map = new HashMap<>();
         map.put("username", userDto.getUsername());
         map.put("link", "https://mikhailpoizon.ru/auth/confirm-mail/" + userDto.getVerify_token());
