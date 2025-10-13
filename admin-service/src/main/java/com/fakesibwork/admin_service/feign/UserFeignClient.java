d54172a1943efcf1abf2b850b46bf10e8af774df
Григорьев Александр Александрович <agrigorev@intdigital.ru>
2025-10-13 11:01:20 +0300

    Feign Client added

---
+++ admin-service/src/main/java/com/fakesibwork/admin_service/feign/UserFeignClient.java
@@ -0,0 +1,14 @@
+package com.fakesibwork.admin_service.feign;
+
+import com.fakesibwork.common.dto.UserDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(value = "UserFeignClient", url = "http://database-service", path = "/api/user")
+public interface UserFeignClient {
+
+    @GetMapping("/all")
+    List<UserDto> getUsers();
+}
--- admin-service/src/main/java/com/fakesibwork/admin_service/service/UserService.java
+++ admin-service/src/main/java/com/fakesibwork/admin_service/service/UserService.java
@@ -1,28 +1,21 @@
 package com.fakesibwork.admin_service.service;
 
-import com.fakesibwork.admin_service.config.RestTemplateConfig;
+import com.fakesibwork.admin_service.feign.UserFeignClient;
 import com.fakesibwork.common.dto.UserDto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.lang.reflect.ParameterizedType;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserService {
 
-    private static final String USER_SERVICE_URL = "http://database-service/api/user/";
+    private final UserFeignClient userFeignClient;
 
-    private RestTemplate restTemplate;
-
-    public UserService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public UserService(UserFeignClient userFeignClient) {
+        this.userFeignClient = userFeignClient;
     }
 
     public List<UserDto> getAllUsers() {
-        List<UserDto> users = restTemplate.getForEntity(USER_SERVICE_URL + "all", List.class).getBody();
-        return users;
+        return userFeignClient.getUsers();
     }
 }
